c05af9b2524d6b187ef60a4e57e12d338844e7fb
Broc <broc.davis@yahoo.com>
2020-01-16 16:49:57 -0600

    added gitignore file and .env to avoid unwanted connection to database outside of the program

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/homeworkDB", { useNewUrlParser: true });
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 app.get("/scrape", function (req, res) {
 
